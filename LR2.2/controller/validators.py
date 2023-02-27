6e1cabed2b8f01bc9270ecda1ce8d6ccf1540dcf
Roman <romchik349@gmail.com>
2023-02-27 10:20:25 +0300

    Initializing GUI

---
+++ LR2.2/controller/validators.py
@@ -0,0 +1,29 @@
+class Validator:
+
+    @staticmethod
+    def validate_name(name: str) -> bool:
+        if not 0 < len(name) <= 100:
+            return False
+        if not name[0].isalpha():
+            return False
+        for i in name:
+            if not (i.isalpha() or i == "-"):
+                return False
+        if not name == name.capitalize():
+            return False
+        return True
+
+    @staticmethod
+    def validate_group_number(group_number: str) -> bool:
+        if not 0 < len(group_number) <= 10:
+            return False
+        for i in group_number:
+            if not i.isdigit():
+                return False
+        return True
+
+    @staticmethod
+    def validate_hours_amount(hours: int) -> bool:
+        if hours > 32000:
+            return False
+        return True
--- LR2.2/view/app.py
+++ LR2.2/view/app.py
@@ -1,7 +1,7 @@
 from kivy.app import App
-from kivy.uix.label import Label
+from kivy.lang.builder import Builder
 
 
 class StudentsApp(App):
     def build(self):
-        return Label(text="Hello world")
+        return Builder.load_file("view/students.kv")
